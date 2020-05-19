33bead7091c9330311c5d9b93d0681d9ad60b970
Berel Levy <berellevy@gmail.com>
2020-05-19 15:30:42 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -2,7 +2,8 @@ def find_max_value(array)
     index = 0
     max_value = 0
     while array[index] do
-        max_value = array[index] if array[index] > max_value
+        if array[index] > max_value
+            max_value = array[index]
         end
         index += 1
     end
